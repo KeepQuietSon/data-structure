e0ea2c762e405dd8ba824fa90d3c11d331d02563
keep <namedpanyue@163.com>
2019-11-23 18:58:40 +0800

    简述栈的链式写法

--- strack.h
+++ strack.h
@@ -16,5 +16,17 @@ bool isEmpty(strackArr &Str);
 //判断栈满
 bool isFull(strackArr &Str);
 
+//出栈
+int pop(strackArr &Str);
+
+//压栈
+int push(strackArr &Str,int data);
 
 #endif
+/*
+链表实现类似于数组实现---若要达成类似于链表实现栈的话需要实现数组空间扩展
+1、链表无判满
+2、链表判空
+3、入栈需要新增节点
+4、出栈则需找到pNext->NULL的节点
+*/
